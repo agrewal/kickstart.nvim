79eb1b78297c40643d925c33b42afe5f442e13ad
Ajeet Grewal <ajeet@sierra.ai>
2024-10-27 20:42:58 -0700

    Adding neo-tree

--- init.lua
+++ init.lua
@@ -627,6 +627,17 @@ require('lazy').setup({
   -- Setup Copilot,
   'github/copilot.vim',
 
+  -- Setup tree view
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+  },
+
   -- Setup test runner
   -- {
   --   'vim-test/vim-test',
